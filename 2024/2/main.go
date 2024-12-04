53e894f51db7e9e4dfac806aea1a341b39e4d3c2
s6227f <erikas.bykovskis@seb.se>
2024-12-04 21:06:04 +0200

    Completed day 2

--- 2024/2/main.go
+++ 2024/2/main.go
@@ -1,9 +1,98 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	file, err := os.Open("values.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	safeCounter := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, " ")
+
+		if len(parts) == 0 {
+			log.Fatal("Invalid line; no entries")
+		}
+
+		if len(parts) == 1 {
+			log.Fatal("Invalid line; single entry")
+		}
+
+		isIncreasing := false
+		isSafe := false
+		for i := range parts {
+			if i == 0 {
+				current, err := strconv.Atoi(parts[i])
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				next, err := strconv.Atoi(parts[i+1])
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if next > current {
+					isIncreasing = true
+				}
+				continue
+			}
+
+			previous, err := strconv.Atoi(parts[i-1])
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			current, err := strconv.Atoi(parts[i])
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Rule 1: Either have decreased or increased
+			isSameValue := previous == current
+			if isSameValue {
+				break
+			}
+
+			hasDecreasedOnIncrease := previous > current && isIncreasing
+			if hasDecreasedOnIncrease {
+				break
+			}
+
+			hasIncreasedOnDecrease := previous < current && !isIncreasing
+			if hasIncreasedOnDecrease {
+				break
+			}
+
+			// Rule 2: Max. difference of abs(3)
+			hasOverDecreased := previous-current < -3
+			hasOverIncreased := previous-current > 3
+			if hasOverIncreased || hasOverDecreased {
+				break
+			}
+
+			isLast := i == len(parts)-1
+			if isLast {
+				isSafe = true
+			}
+		}
+
+		if isSafe {
+			safeCounter++
+		}
+	}
+
+	fmt.Println("Answer:", safeCounter)
 }
